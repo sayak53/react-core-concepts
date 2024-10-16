b68b678b1d28e27244b2bed1227b9f9d74bea6e9
sayak53 <sayakchakraborty928@gmail.com>
2024-10-16 20:00:45 +0530

    dynamic data displayed

--- src/User.jsx
+++ src/User.jsx
@@ -1,7 +1,8 @@
 export default function User({user}){
-    const {name,email} = user
+    const {name,email,id} = user
     return(
         <div>
+            <h4>Id:{id}</h4>
             <h4>Name:{name}</h4>
             <h4>Email:{email}</h4>
         </div>
